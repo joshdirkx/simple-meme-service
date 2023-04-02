aef3649761a68b493bd1e6a412fc9897bf215e9c
Josh Dirkx <joshdirkx@gmail.com>
2023-04-01 21:42:50 -0700

    start

---
+++ lambda/delete/index.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+const bucketName = process.env.BUCKET_NAME;
+
+exports.handler = async (event) => {
+  try {
+    s3.deleteObject({
+      Bucket: bucketName,
+      Key: event.pathParameters.imageKey
+    });
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: 'Image deleted',
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: 'Internal Server Error',
+      }),
+    };
+  };
+};
--- lib/simple-meme-service-stack.ts
+++ lib/simple-meme-service-stack.ts
@@ -1,16 +1,69 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Architecture, Code, Function, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+var path = require('path');
 
 export class SimpleMemeServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // creates a bucket in which images will be stored
+    const bucket = new Bucket(this, 'simpleMemeServiceBucket', {
+      versioned: false
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleMemeServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+    // creates a lambda function that will place images in the bucket
+    const uploadImageLambda = new Function(this, 'simpleMemeServiceImageUploadLambda', {
+      code: Code.fromAssetImage(path.join(__dirname, '..', 'lambda', 'upload')),
+      handler: Handler.FROM_IMAGE,
+      runtime: Runtime.FROM_IMAGE,
+      architecture: Architecture.ARM_64,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      }
+    });
+
+    // creates a lambda function that will get images from the bucket
+    const getImageLambda = new Function(this, 'simpleMemeServiceGetImageLambda', {
+      code: Code.fromAssetImage(path.join(__dirname, '..', 'lambda', 'upload')),
+      handler: Handler.FROM_IMAGE,
+      runtime: Runtime.FROM_IMAGE,
+      architecture: Architecture.ARM_64,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      }
+    });
+
+    // allow the lambda function to put images in the bucket
+    bucket.grantPut(uploadImageLambda);
+
+    // create a new api
+    const api = new RestApi(this, 'simpleMemeServiceApi');
+
+    // add /images to the api
+    const imagesApi = api.root.addResource('images');
+
+    // declare a new proxy lambda integration for uploading images to the s3 bucket
+    const uploadImageLambdaIntegration = new LambdaIntegration(uploadImageLambda, {
+      proxy: true,
+    });
+    
+    // declare a new proxy lambda integration for retrieving images from the s3 bucket
+    const getImageLambdaIntegration = new LambdaIntegration(getImageLambda, {
+      proxy: true,
+    });
+
+    // declare post /images with the upload image lambda as the handler
+    imagesApi.addMethod('POST', uploadImageLambdaIntegration);
+    // delcares get /images with the get image lambda as the handler
+    imagesApi.addMethod('GET', getImageLambdaIntegration);
+
+    new CfnOutput(this, 'simpleMemeServiceApiUrl', {
+      value: api.url,
+    });
+  };
+};
