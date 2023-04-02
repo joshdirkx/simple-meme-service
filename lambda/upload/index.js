09edb0875d192dfaecd71240695759f78767e2b5
Josh Dirkx <joshdirkx@gmail.com>
2023-04-02 13:08:36 -0700

    stuff

--- lambda/upload/index.js
+++ lambda/upload/index.js
@@ -2,4 +2,31 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3();
 const bucketName = process.env.BUCKET_NAME;
 
+exports.handler = async (event) => {
+  try {
+    console.log(event);
 
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: 'Image added to bucket',
+        imageUrl: 'url',
+      })
+    }
+  } catch (error) {
+    console.log(error);
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: 'Internal Server Error',
+      }),
+    };
+  };
+};
