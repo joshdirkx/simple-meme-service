6537dc42203f44312c8722cec8dda62141356a06
Josh Dirkx <joshdirkx@gmail.com>
2023-04-01 21:15:29 -0700

    Initial commit

---
+++ test/simple-meme-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleMemeService from '../lib/simple-meme-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-meme-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleMemeService.SimpleMemeServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
